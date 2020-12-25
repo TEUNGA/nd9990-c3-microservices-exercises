4c17fdb13d61e95f54b28a744b9979f46b454000
root <cesaime18@gmail.com>
2020-12-25 02:28:20 +0100

    mon premier commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=database-10
+export AWS_BUCKET=microservice10
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=
+export JWT_SECRET=hello
+export URL=http://localhost:8100
